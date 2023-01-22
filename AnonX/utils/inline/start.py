00b16c56b12d14b23638754a0e921088bfd38454
Fᴀʙɪɴᴏ <116137433+FabinoXD@users.noreply.github.com>
2023-01-23 02:02:54 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -36,7 +36,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="⁂ Hᴇʟᴘ ⁂", callback_data="settings_back_helper"
+                text="⁂ Cᴏᴍᴍᴀɴᴅs ⁂", callback_data="settings_back_helper"
             ),
             InlineKeyboardButton(
                 text="♡ Oᴡɴᴇʀ ♡", user_id=OWNER
